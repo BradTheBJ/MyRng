8a555ff1934b400c1e093a8bf9fc75be09ce1836
BradTheBJ <bradbenjoya6@gmail.com>
2025-02-21 21:23:28 +0200

    Update MyRng.py

--- MyRng.py
+++ MyRng.py
@@ -12,6 +12,15 @@ good = higest_raritiy/8
 
 while True:
     random.randint(1, higest_raritiy)
+
     if  rng >= 4:
         print("common")
+
+    if  rng >= 2:
+        print("uncommon")
+
+    if  rng >= 1:
+        print("good")
+
+
     
