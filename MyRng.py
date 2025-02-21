24d22044c415978c1f9b8e94fc35224ba3ee7a64
BradTheBJ <bradbenjoya6@gmail.com>
2025-02-22 00:04:57 +0200

    Update MyRng.py

--- MyRng.py
+++ MyRng.py
@@ -13,6 +13,7 @@ good = int(higest_raritiy / 8)
 great = int(higest_raritiy / 16)
 amazing = int(higest_raritiy / 32)
 
+
 while True:
     time.sleep(1)
   
@@ -29,11 +30,15 @@ while True:
 
     if rng > common:
         print("common")
+
     elif rng > uncommon:
         print("uncommon")
+
     elif rng > good:
         print("good")
+        
     elif rng > great:
         print("great")
+
     else:
         print("amazing")
