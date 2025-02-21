b25fc83c2d69279ef41d3fd5aa53a89dca56ddf2
BradTheBJ <bradbenjoya6@gmail.com>
2025-02-21 21:22:06 +0200

    finaly working

---
+++ MyRng.py
@@ -0,0 +1,17 @@
+import pygame
+import math
+import random
+
+
+higest_raritiy = 8
+rng = random.randint(1, higest_raritiy)
+
+common = higest_raritiy/2
+uncommon = higest_raritiy/4
+good = higest_raritiy/8
+
+while True:
+    random.randint(1, higest_raritiy)
+    if  rng >= 4:
+        print("common")
+    
