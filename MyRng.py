6d33e32c4e7f95673831c872a90fd2244fae59de
BradTheBJ <bradbenjoya6@gmail.com>
2025-02-21 21:41:39 +0200

    Update MyRng.py

--- MyRng.py
+++ MyRng.py
@@ -3,27 +3,34 @@ import math
 import random
 import time
 
-
 higest_raritiy = 8
-rng = random.randint(1, higest_raritiy)
 
-common = higest_raritiy/2
-uncommon = higest_raritiy/4
-good = higest_raritiy/8
+pity = 0
+
+common = int(higest_raritiy / 2)
+uncommon = int(higest_raritiy / 4)
+good = int(higest_raritiy / 8)
 
 while True:
-    random.randint(1, higest_raritiy)
-    print(rng)
+    pity += 1
+
+    if pity == 10:
+        print("2x luck")
+        rng = random.randint(1, int(higest_raritiy / 2))
+
+    else:
+        rng = random.randint(1, higest_raritiy)
+
     time.sleep(1)
 
-    if  rng >= 4:
-        print("common")
+    if rng >= 4:
+        print("common 1/", common)
 
-    elif  rng >= 2:
-        print("uncommon")
+    elif rng >= 2:
+        print("uncommon 1/", uncommon)
 
-    elif  rng >= 1:
-        print("good")
+    elif rng >= 1:
+        print("good 1/", good)
 
 
     
